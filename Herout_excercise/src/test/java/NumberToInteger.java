b381b1f795b0a128dbfbcb0f264e85195e114e31
Zuzana Ondrackova <ondrac.zuzana@gmail.com>
2025-01-05 17:54:13 +0100

    Adding two exercises.

---
+++ Herout_excercise/src/test/java/NumberToInteger.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NumberToInteger {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double number = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                System.out.println("Write some number: ");
+                number = scanner.nextDouble();
+                validInput = true;
+            } catch (Exception e) {
+                System.out.println("Invalid input! Please enter a valid number.");
+                scanner.nextLine();
+            }
+        }
+        int intNumber = (int) Math.floor(number);
+
+        System.out.println("The integer part is: " + intNumber);
+        scanner.close();
+    }
+}
---
+++ Herout_excercise/src/test/java/UpperCaseToLowerCase.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class UpperCaseToLowerCase {
+    public static void main (String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Write three Upper case letters: ");
+
+    String input = scanner.next();
+
+        if (input.length() == 3) {
+            for (int i = 0; i < 3; i++) {
+                char upperCaseLetter = input.charAt(i);
+                char lowerCaseLetter = Character.toLowerCase(upperCaseLetter);
+            System.out.print(lowerCaseLetter);
+        }
+    } else {
+        System.out.println("You must enter exactly three uppercase letters.");
+    }
+        scanner.close();
+    }
+}
